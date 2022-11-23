b609de4dbb3cd3bb8818b271ac453eb546b731e3
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-23 23:15:38 +0330

    account module app created

--- better_home_api/settings.py
+++ better_home_api/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # our applications
+    'account_module',
 ]
 
 MIDDLEWARE = [
