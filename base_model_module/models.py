53dad095c3974865e3acf5e5a92323988309f8e5
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-24 02:13:34 +0330

    base_model_module model created

--- base_model_module/models.py
+++ base_model_module/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModelManager(models.Manager):
+    def all(self):
+        return self.get_queryset().filter(is_delete=False)
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True, verbose_name='فعال بودن / نبودن')
+    is_delete = models.BooleanField(default=False, verbose_name='حدف شده / نشده')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    last_updated_time = models.DateTimeField(auto_now=True, verbose_name='تاریخ آخرین اپدیت')
+
+    objects = BaseModelManager()
+    
+    class Meta:
+        abstract = True
