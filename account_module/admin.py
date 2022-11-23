1747d1376e0552750dab7deb8703c94b7392d0d1
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-23 23:58:04 +0330

    User django panel admin created

--- account_module/admin.py
+++ account_module/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+UserAdmin.fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "country_code", "phone_number", "is_phone_number_verified")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+)
+
+UserAdmin.list_display = ('username', 'country_code', 'phone_number',
+                          'is_phone_number_verified', 'first_name', 'last_name',
+                          'is_staff', 'is_active')
+
+admin.site.register(User, UserAdmin)
