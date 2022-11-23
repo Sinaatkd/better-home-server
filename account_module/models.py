9a89856c8d8a5c8ee010dd1385429af1a9180fbf
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-23 23:34:35 +0330

    based user model created

--- account_module/models.py
+++ account_module/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    country_code = models.IntegerField(default=98, verbose_name='کد کشور')
+    phone_number = models.BigIntegerField(verbose_name='شماره تلفن')
+    
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
