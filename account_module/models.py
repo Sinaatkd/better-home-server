e58816a7d62e33c224495bd86da50dccbab516b2
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-23 23:56:17 +0330

    is_phone_number_verified field added in to User model

--- account_module/models.py
+++ account_module/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     country_code = models.IntegerField(default=98, verbose_name='کد کشور')
     phone_number = models.BigIntegerField(verbose_name='شماره تلفن')
+    is_phone_number_verified = models.BooleanField(default=False, verbose_name='شماره تلفن تایید شده/نشده')
     
     class Meta:
         verbose_name = 'کاربر'
