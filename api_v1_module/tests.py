a44d550c362aa40f506582d01f1c48087f908531
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-24 17:47:51 +0330

    SendVerificationCodeTest completed

--- api_v1_module/tests.py
+++ api_v1_module/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+
+client = Client()
+
+class SendVerificationCodeTest(TestCase):
+    def test_correct_request(self):
+        test_phone_number = 9100000000
+        payload = {'phone_number': test_phone_number}
+        res = client.post(reverse('SendVerificationCodeAPI'), data=payload)
+        expected_response = {
+            'status': 'ok',
+            'message': 'code sent'
+        }
+        self.assertEqual(res.json(), expected_response)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+    
+    def test_incorrect_request(self):
+        test_phone_number = 9100000000
+        res = client.post(reverse('SendVerificationCodeAPI'))
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
