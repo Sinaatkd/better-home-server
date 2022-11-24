4b898eac7683ce755f4e024861d75f0b1af51c72
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-24 18:15:30 +0330

    <<create superuser>> command created

---
+++ account_module/management/commands/create_superuser.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'create an admin user'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs='+', type=str)
+        parser.add_argument('password', nargs='+', type=str)
+        parser.add_argument('phone_number', nargs='+', type=str)
+    
+    
+    def handle(self, *args, **kwargs):
+        username = kwargs['username'][0]
+        password = kwargs['password'][0]
+        phone_number = kwargs['phone_number'][0]
+        user = User(username=username, phone_number=phone_number)
+        user.set_password(password)
+        try:
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'User "{username}" Created'))
+        except:
+            raise CommandError('Phone Number or username Already taken')
