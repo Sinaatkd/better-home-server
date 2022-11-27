69cb861864b4091d6d697ae616df41e6e218bc1d
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-27 16:59:35 +0330

    sign in tests written

--- api_v1_module/tests.py
+++ api_v1_module/tests.py
@@ -1,11 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
 from rest_framework import status
 
-from api_v1_module.serializers import SendVerificationCodeSerializer
+from api_v1_module.serializers import (
+    SendVerificationCodeSerializer,
+    SignInSerializer)
+
+from account_module.models import VerificationCode
 
 client = Client()
+User = get_user_model()
 
 
 class SendVerificationCodeTest(TestCase):
@@ -18,9 +24,50 @@ class SendVerificationCodeTest(TestCase):
             serializer.save()
             self.assertEqual(res.json(), serializer.data)
             self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-    
+
     def test_incorrect_request(self):
-        test_phone_number = 9100000000
         res = client.post(reverse('send_verification_code_api'))
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
+
+class SignInTest(TestCase):
+    def setUp(self) -> None:
+        self.phone_number = 9120000000
+        self.user = User.objects.get_or_create(phone_number=self.phone_number)
+        self.verification_code = VerificationCode.objects.create_verification_code(user=self.user)
+
+    def test_success_sign_in(self):
+        payload = {
+            'phone_number': self.phone_number,
+            'verification_code': self.verification_code.code
+        }
+        res = client.post(reverse('sign_in_api'), data=payload)
+        serializer = SignInSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+            self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_found_sign_in(self):
+        not_registered_phone_number = 9212122413
+        payload = {
+            'phone_number': not_registered_phone_number,
+            'verification_code': self.verification_code.code
+        }
+        res = client.post(reverse('sign_in_api'), data=payload)
+        serializer = SignInSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.json(), serializer.errors)
+
+    def test_incorrect_verification_code(self):
+        payload = {
+            'phone_number': self.phone_number,
+            'verification_code': 1111
+        }
+        res = client.post(reverse('sign_in_api'), data=payload)
+        serializer = SignInSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.json(), serializer.errors)
--- api_v1_module/views.py
+++ api_v1_module/views.py
@@ -7,7 +7,7 @@ from .serializers import (SendVerificationCodeSerializer, SignInSerializer)
 
 class SendVerificationCodeAPI(GenericAPIView):
     serializer_class = SendVerificationCodeSerializer
-    
+
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
@@ -15,12 +15,13 @@ class SendVerificationCodeAPI(GenericAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class SignInAPI(GenericAPIView):
     serializer_class = SignInSerializer
-    
+
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():            
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
