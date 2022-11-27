fb24ac38aad900665b49fa5327c09757e8f4046d
Sina Amini <general.sina.amini.20@gmail.com>
2022-11-27 21:30:22 +0330

    is_consultant added to user admin (django admin default)

--- account_module/admin.py
+++ account_module/admin.py
@@ -4,33 +4,35 @@ from django.utils.translation import gettext_lazy as _
 
 from .models import User, VerificationCode
 
-
 UserAdmin.fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "country_code", "phone_number", "is_phone_number_verified")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    (None, {"fields": ("username", "password")}),
+    (_("Personal info"),
+     {"fields": ("first_name", "last_name", "country_code", "phone_number", "is_phone_number_verified")}),
+    (
+        _("Permissions"),
+        {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                'is_consultant',
+                "groups",
+                "user_permissions",
+            ),
+        },
+    ),
+    (_("Important dates"), {"fields": ("last_login", "date_joined")}),
 )
 
 UserAdmin.list_display = ('username', 'country_code', 'phone_number',
                           'is_phone_number_verified', 'first_name', 'last_name',
-                          'is_staff', 'is_active')
+                          'is_active', 'is_staff', 'is_consultant')
 
 admin.site.register(User, UserAdmin)
 
 
 class VerificationCodeAdmin(admin.ModelAdmin):
     list_display = ('user', 'code', 'expire_time')
+
 
 admin.site.register(VerificationCode, VerificationCodeAdmin)
