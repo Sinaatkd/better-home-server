031c1779a7a2e587a62710cfa8a0e11c6918ff2b
Sina Amini <general.sina.amini.20@gmail.com>
2022-12-08 11:22:18 +0330

    estate_module verbose_name was set

--- estate_module/apps.py
+++ estate_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EstateModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'estate_module'
+    verbose_name = 'ماژول ملک ها'
