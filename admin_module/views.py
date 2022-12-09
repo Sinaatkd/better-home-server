e74ceed22176de3abd8244249b87484caaac42bf
Sina Amini <general.sina.amini.20@gmail.com>
2022-12-09 18:53:23 +0330

    admin panel top cards added

--- admin_module/views.py
+++ admin_module/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+from estate_module.models import Estate
+
+User = get_user_model()
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    all_users = User.objects.all()
+    all_users_count = all_users.count()
+    all_consultants_count = all_users.filter(is_consultant=True).count()
+    all_estates_ads = Estate.objects.all().count()
+    
+    context = {
+        'all_users_count': all_users_count,
+        'all_consultants_count': all_consultants_count,
+        'all_estates_ads': all_estates_ads,
+    }
+    return render(request, 'dashboard.html', context)
