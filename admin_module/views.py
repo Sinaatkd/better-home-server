dc96b4911571f8652bb0e313c9b1b1b4784f3368
Sina Amini <general.sina.amini.20@gmail.com>
2022-12-29 17:51:43 +0330

    show CPU, MEM usage on dashboard page

--- admin_module/views.py
+++ admin_module/views.py
@@ -1,3 +1,4 @@
+import psutil
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 
@@ -12,11 +13,15 @@ def dashboard(request):
     all_users_count = all_users.count()
     all_consultants_count = all_users.filter(is_consultant=True).count()
     all_estates_ads = Estate.objects.all().count()
+    memory_percent_usage = psutil.virtual_memory()[2]
+    cpu_percent_usage = psutil.cpu_percent()
 
     context = {
         'all_users_count': all_users_count,
         'all_consultants_count': all_consultants_count,
         'all_estates_ads': all_estates_ads,
+        'memory_percent_usage': memory_percent_usage,
+        'cpu_percent_usage': cpu_percent_usage,
     }
     return render(request, 'dashboard.html', context)
 
